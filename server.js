2325a6bb450d7bd49b6890b90c90c710108ef261
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2024-08-14 14:10:24 +0100

    create server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() =>{console.log('MongoDB connected')})
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Personal Finance Management Tool API')
+});
+
+// app.use('/api/auth', require('./routes/auth'));
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`)
+})
