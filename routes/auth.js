44a7cf56c6aa19860d9cb9c77e7c9b49736b06ad
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2024-08-28 16:54:00 +0100

    update to use controllers

--- routes/auth.js
+++ routes/auth.js
@@ -1,54 +1,11 @@
 const express = require('express');
-const User = require('../models/User');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
+const authControllers = require('../controllers/authControllers')
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
-    const { name, email, password} = req.body;
-    try {
-        let user = await User.findOne({ email });
-        if (user) {
-            return res.status(400).json({ msg: 'User already exists' });
-        }
-        user = new User({name, email, password});
-        await user.save();
+router.post('/register', authControllers.regiser);
 
-        const payload = { user: { id: user.id } };
-        jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 3600 }, (err, token) => {
-            if (err) throw err;
-            res.json({ token });
-        });
-    }
-    catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-});
+router.post('/login', authControllers.login);
 
-router.post('/login', async (req, res) => {
-    const { email, password} = req.body;
-    try {
-        let user = await User.findOne({ email });
-        if (!user) {
-            return res.status(400).json({ msg: 'User does not exist' });
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ msg: 'Invalid Credentials' });
-        }
-
-        const payload = { user: { id: user.id } };
-        jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 3600 }, (err, token) => {
-            if (err) throw err;
-            res.json({ token });
-        });
-    }catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-});
+router.post('/resetPassword', authControllers.resetPassword);
 
 module.exports = router;
