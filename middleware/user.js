3a612482de672b87d6bd7089eba046f5716bc4b3
Darasimi <topedarasimi5@gmail.com>
2024-09-29 19:39:56 +0100

    changes

--- middleware/user.js
+++ middleware/user.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 const fetchUserProfile = async (req, res) => {
     try {
         const userId = req.user.id;
-        res.json(user);
+        res.json(userId);
     }
     catch (error) {
         console.error(error.message);
